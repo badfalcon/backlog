0acc80818425d1b4b9992071301832915bffcc5a
GitHub Action <action@github.com>
2023-10-25 16:02:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/badfalcon/backlog/MyBundle.kt
+++ src/main/kotlin/com/github/badfalcon/backlog/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.badfalcon.backlog
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/badfalcon/backlog/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/badfalcon/backlog/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.badfalcon.backlog.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.badfalcon.backlog.MyBundle
+import com.github.badfalcon.backlog.services.MyProjectService
 import javax.swing.JButton
 
 
